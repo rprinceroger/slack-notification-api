1c5093fc7acd8f3c914a3e116e04bbe341bed77d
Prince Roger Robielos <prince.robielos@gmail.com>
2023-12-01 21:18:50 +0800

    Add try catch

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const axios = require('axios'); // Import Axios for making HTTP requests
 
 const app = express();
 const port = 3000;
@@ -10,7 +9,7 @@ require('dotenv').config();
 app.use(bodyParser.json());
 
 // Your API endpoint for sending messages using POST
-app.post('/send-message', async (req, res) => {
+app.post('/send-message', (req, res) => {
   try {
     const { token, channel, text } = req.body;
 
@@ -20,40 +19,23 @@ app.post('/send-message', async (req, res) => {
 
     console.log(`Sending message to channel ${process.env.SLACK_CHANNEL}: ${text}`);
 
-    // Make a request to Slack's chat.postMessage API
-    const response = await axios.post(
-      'https://slack.com/api/chat.postMessage',
-      {
-        channel: process.env.SLACK_CHANNEL,
-        text: text,
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${process.env.SLACK_BOT_TOKEN}`, // Use your Slack Bot token here
-        },
-      }
-    );
-
-    if (!response.data.ok) {
-      // Handle Slack API error
-      console.error('Slack API Error:', response.data.error);
-      return res.status(500).json({ error: 'Failed to send message to Slack' });
-    }
-
     res.json({ message: 'Message sent successfully' });
   } catch (error) {
-    // Handle unexpected errors
-    console.error('Unexpected Error:', error.message);
+    console.error('Error handling /send-message:', error.message);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
 // Your OAuth installation endpoint for handling GET requests
 app.get('/install', (req, res) => {
-  // Redirect to Slack's OAuth authorization URL
-  const redirectUrl = `https://slack.com/oauth/v2/authorize?client_id=${process.env.SLACK_CLIENT_ID}&scope=${process.env.SLACK_SCOPES}&user_scope=${process.env.SLACK_USER_SCOPES}&redirect_uri=${process.env.SLACK_REDIRECT_URI}`;
-  res.redirect(302, redirectUrl);
+  try {
+    // Redirect to Slack's OAuth authorization URL
+    const redirectUrl = `https://slack.com/oauth/v2/authorize?client_id=${process.env.SLACK_CLIENT_ID}&scope=${process.env.SLACK_SCOPES}&user_scope=${process.env.SLACK_USER_SCOPES}&redirect_uri=${process.env.SLACK_REDIRECT_URI}`;
+    res.redirect(302, redirectUrl);
+  } catch (error) {
+    console.error('Error handling /install:', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 app.listen(port, () => {
